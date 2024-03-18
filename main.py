42a510814d1939a409754845681086b574545eed
Maxime Delporte <62073284+MaximeDelporte@users.noreply.github.com>
2024-03-18 11:47:16 +0100

    Finish print_report function.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def count_words(book) -> int:
+def get_words_count_from(book) -> int:
     return len(book.split())
 
-def count_letters(book):
+
+def get_letters_count_from(book):
     lowered_string = book.lower()
     words = lowered_string.split()
 
@@ -12,27 +13,42 @@ def count_letters(book):
         for letter in letters:
             all_letters.append(letter)
 
-    count_letters = {}
+    dictionary_letters = {}
 
     for letter in all_letters:
-        if letter in count_letters:
-            dict[letter] += 1
-        else:
-            dict[letter] = 1
+        if not letter.isalpha():
+            continue
 
-    print(count_letters)
+        if letter in dictionary_letters:
+            dictionary_letters[letter] += 1
+        else:
+            dictionary_letters[letter] = 1
+
+    sorted_array = sorted(dictionary_letters.items(), key=lambda x:x[1])
+    ordered_array = sorted_array[::-1]
+    return ordered_array
+
 
 def print_report(path, book):
     print(f"--- Begin report of {path} ---")
-    words = count_words(book)
-    print(f"{words} words found in the document")
+    words = get_words_count_from(book)
+    print(f"{words} words found in the document\n")
+
+    letters_count = get_letters_count_from(book)
+    for key, value in letters_count:
+        time_word = "times"
+        if value == 1:
+            time_word = "time"
+
+        print(f"The '{key}' character was found {value} {time_word}")
+
+    print("--- End report ---")
 
 def main():
     path = "./books/frankenstein.txt"
-    book = ""
     with open(path) as f:
         book = f.read()
         print_report(path, book)
 
-main()
 
+main()
